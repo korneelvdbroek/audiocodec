* fixed bug in mdct amplitude normalization

audio_encoder.py
@@ -16,11 +16,10 @@ import matplotlib.animation as animation
 
 from audiocodec import codec_utils, psychoacoustic, mdct, codec
 
-# todo: 1. fix normalization issues in mdct (try different filter_n & see impact on mdct amplitudes)
-# todo: 2. pyschoacoustic model:
-# todo: 2.1 move to [batches_n, blocks_n, filters_n, channels_n] format in psychoacoustic
-# todo: 2.2 move all psychoacoustic filter to the normalized mdct space (speedup...)
-# todo: 2.3 pull psychoacoustic filter in from trancegan code
+# todo: 1. pyschoacoustic model:
+# todo: 1.1 move to [batches_n, blocks_n, filters_n, channels_n] format in psychoacoustic
+# todo: 1.2 move all psychoacoustic filter to the normalized mdct space (speedup...)
+# todo: 1.3 pull psychoacoustic filter in from trancegan code
 
 CPU_ONLY = False
 DEBUG = False
@@ -88,8 +87,8 @@ def clip_wav(start, stop, wave_data, sample_rate):
 
 def test_mdct(sample_rate, wave_data):
     # plot spectrum
-    filter_bands_n = 256
-    mdct_setup = mdct.setup(filter_bands_n)
+    filter_bands_n = 90
+    mdct_setup = mdct.setup(filter_bands_n, window_type='sine')
 
     wave_data = wave_data[:, 0:filter_bands_n * int(wave_data.shape[1] / filter_bands_n)]
 
@@ -99,11 +98,6 @@ def test_mdct(sample_rate, wave_data):
     fig, ax = plt.subplots(nrows=1)
     codec_utils.plot_spectrogram(ax, spectrum)
     plt.show()
-
-    # print(wave_data) # , summarize=181)
-    # print(wave_reproduced) #, summarize=181)
-    tf.print(wave_data - wave_reproduced[:, filter_bands_n:-filter_bands_n], summarize=181)
-    tf.print(tf.reduce_max(tf.abs(wave_data - wave_reproduced[:, filter_bands_n:-filter_bands_n])))
 
     play_wav(wave_reproduced.numpy(), sample_rate)
     save_wav('./data/asot_02_cosmos_sr8100_118_128_reconstructed.wav', wave_reproduced.numpy(), sample_rate)
@@ -126,6 +120,10 @@ def main():
 
     test_mdct(sample_rate, wave_data)
 
+    exit()
+
+    # todo: code below is broken, while psychoacoustic module is being refactored...
+    
     # play_wav(wave_data, sample_rate)
 
     # plot spectrum

audiocodec/mdct.py
@@ -21,7 +21,7 @@ def setup(filters_n=1024, window_type='vorbis'):
     todo: work out polymatmul(x_pp, H) and polymatmul(y, H_inv) in more efficient way
 
     :param filters_n:   number of filter bands of the filter bank (needs to be even)
-    :param window_type: None, 'sin' or 'vorbis' (default) to select window type
+    :param window_type: None, 'sine' or 'vorbis' (default) to select window type
     :return:            tuple with pre-computed required for encoder and decoder
     """
     assert (filters_n % 2) == 0, "number of filters used in mdct transformation needs to be even"
@@ -146,13 +146,13 @@ def _filter_window_matrix(filters_n, window_type="vorbis"):
     synthesis base-band impulse responses. Hence has det 1 or -1.
 
     :param filters_n:   number of MDCT filters (needs to be even!)
-    :param modified:    if True (default) then does a MDCT, if False does a DCT
+    :param window_type: None, 'sine' or 'vorbis' (default) to select window type
     :return:            F of shape (filters_n, filters_n)
     """
-    if window_type.lower == 'sin':
+    if window_type.lower() == 'sine':
         # Sine window:
         filter_bank_windows = tf.sin(math.pi / (2 * filters_n) * (tf.range(0.5, int(1.5 * filters_n) + 0.5)))
-    elif window_type == 'vorbis':
+    elif window_type.lower() == 'vorbis':
         filter_bank_windows = tf.sin(
             math.pi / 2. * tf.sin(math.pi / (2. * filters_n) * tf.range(0.5, int(1.5 * filters_n) + 0.5)) ** 2)
     else:
@@ -306,8 +306,9 @@ def normalize_mdct(mdct_amplitudes):
     :param mdct_amplitudes: -inf..inf  [channels_n, #blocks, filter_bands_n]
     :return:                -1..1      [channels_n, #blocks, filter_bands_n]
     """
+    tf.print('normalizing...')
     mdct_norm = tf.sign(mdct_amplitudes) / 6. * \
-                tf.math.log(tf.abs(mdct_amplitudes) * 10.**6 / math.sqrt(2.*mdct_amplitudes.shape[2]) + _LOG_EPS) / \
+                tf.maximum(tf.math.log(tf.abs(mdct_amplitudes) * 10.**6 / math.sqrt(2.*mdct_amplitudes.shape[2])), 0.0) / \
                 tf.math.log(10.)
 
     return tf.clip_by_value(mdct_norm, -1., 1.)

tests/mdct.py
@@ -16,19 +16,23 @@ def sine_wav(amplitude, frequency, sample_rate=44100, duration_sec=2.0):
 
 
 class TestMDCT(unittest.TestCase):
-    def test_idempotence(self):
+    def test_inverse_identity(self):
+        """Check that x = MDCT^{-1}(MDCT(x))"""
+        # mdct setup
+        filters_n = 256
+        mdct_setup = mdct.setup(filters_n)
+
+        # create test signal
         wave_data = sine_wav(0.8, 880, sample_rate=16000, duration_sec=1.)
-
-        filter_bands_n = 256
-        mdct_setup = mdct.setup(filter_bands_n)
-
-        wave_data = wave_data[:, 0:filter_bands_n * int(wave_data.shape[1] / filter_bands_n)]
+        wave_data = wave_data[:, 0:filters_n * int(wave_data.shape[1] / filters_n)]
         wave_data = tf.convert_to_tensor(wave_data, dtype=tf.float32)
 
+        # transform and go back
         spectrum = mdct.transform(wave_data, mdct_setup)
         wave_reproduced = mdct.inverse_transform(spectrum, mdct_setup)
 
-        zero = tf.reduce_max(tf.abs(wave_data - wave_reproduced[:, filter_bands_n:-filter_bands_n]))
+        # compare original x with y = MDCT^{-1}(MDCT(x))
+        zero = tf.reduce_max(tf.abs(wave_data - wave_reproduced[:, filters_n:-filters_n]))
 
         self.assertLess(zero, EPS, "Should be zero")
 